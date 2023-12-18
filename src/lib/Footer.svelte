3ec59687fcc96e5a78f06c2a96e516619aca54dd
zackey1514 <miyazaki1514@gmail.com>
2023-12-18 23:28:40 +0900

    update. dotenv

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,13 +1,21 @@
 <script>
+	import { PUBLIC_TWITTER_ID } from '$env/static/public';
 	import { Footer, FooterCopyright, FooterIcon } from 'flowbite-svelte';
-	import { HeadphonesSolid, TwitterSolid } from 'flowbite-svelte-icons';
+	import { GithubSolid, HeadphonesSolid, TwitterSolid } from 'flowbite-svelte-icons';
 </script>
 
 <Footer>
 	<div class="py-6 px-4 md:flex md:items-center md:justify-between">
 		<p class="text-gray-500 text-sm sm:text-center">Made by @zackey878</p>
 		<div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center md:mt-0">
-			<FooterIcon href="https://twitter.com/zackey878" target="_blank" rel="noopener noreferrer">
+			<FooterIcon
+				href="https://github.com/zackey1514/spotify-genre-search"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<GithubSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900" />
+			</FooterIcon>
+			<FooterIcon href="https://twitter.com/{PUBLIC_TWITTER_ID}" target="_blank" rel="noopener noreferrer">
 				<TwitterSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900" />
 			</FooterIcon>
 			<FooterIcon
