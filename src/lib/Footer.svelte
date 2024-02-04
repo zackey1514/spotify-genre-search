9bc0cfd336177c4df6071f0f30666efbbde8ab17
zackey1514 <miyazaki1514@gmail.com>
2024-02-04 20:40:23 +0900

    update. footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,7 +5,7 @@
 
 <Footer>
   <div class="py-6 px-4 md:flex md:items-center md:justify-between">
-    <p class="text-gray-500 text-sm sm:text-center">Made by @zackey878</p>
+    <p class="text-gray-500 text-sm sm:text-center">Made by @zackey1514</p>
     <div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center md:mt-0">
       <FooterIcon
         href="https://github.com/zackey1514/spotify-genre-search"
